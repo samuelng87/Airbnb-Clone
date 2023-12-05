7e2daf112e26b82b63b849d843cebb54eee47eb9
sam <samuel6249ng@gmail.com>
2023-12-05 21:50:32 +1100

    map with CSS  manual render

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,7 +10,7 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
-import Map from "../Map"
+import dynamic from "next/dynamic";
 
 enum STEPS {
      CATEGORY = 0,
@@ -51,6 +51,10 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+
+    const Map = useMemo(()=> dynamic(()=> import('../Map'), { 
+        ssr:false
+    }),[location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -129,7 +133,9 @@ const RentModal = () => {
                     value={location}
                     onChange={(value) => setCustomValue('location', value)}
                 />
-                <Map />
+                <Map 
+                center={location?.latlng}
+                />
             </div>
 
         )
