a54f23151c493ad5e947f22e92f84cb611d45b92
sam <samuel6249ng@gmail.com>
2023-12-12 00:22:26 +1100

    fetching listing with server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
 import Image from 'next/image'
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
+
 
 export default function Home() {
   return (
-    <div className="text-rose-500 text-2xl">Loading...</div>
-  )
+      <ClientOnly>
+        <Container>
+          <div className="
+            pt-24
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          ">
+
+          </div>
+        </Container>
+      </ClientOnly>   
+    )
 }
